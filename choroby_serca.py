2f28261abcbfab89e7fe83557bb67a89c33e2896
Agstarte <aga1999.1@gmail.com>
2021-11-08 17:34:05 +0100

    dodano wiele klasyfikatorów

--- choroby_serca.py
+++ choroby_serca.py
@@ -2,13 +2,30 @@ import pickle
 
 import numpy as np
 
-test_data = [[56, 1, 4, 140, 207, 0, 2, 138, 1, 19.0, 1, 1, 7]]
-
-clf = pickle.load(open('klasyfikator_chorob_serca.pkl', 'rb'))
-prognosis = clf.predict(test_data)[0]
 label = {
-    1: 'Negatywna',
-    2: 'Pozytywna'
+        1: 'Negatywna',
+        2: 'Pozytywna'
+    }
+
+test_data = [[43, 1, 4, 140, 207, 0, 2, 138, 1, 19.0, 1, 1, 7]]
+classifiers_pkl = {
+    'KNN': 'heart_KNN.pkl',
+    'MLP': 'heart_MLP.pkl',
+    'NB': 'heart_NB.pkl',
+    'SVC': 'heart_SVC.pkl',
+
 }
-print(f'Prognoza: {label[prognosis]}\n'
-      f'Prawdopodobieństwo: {np.max(clf.predict_proba(test_data)) * 100}')
+
+out = dict()
+
+for clf in classifiers_pkl:
+    out[clf] = dict()
+
+    model = pickle.load(open(classifiers_pkl[clf], 'rb'))
+    prognosis = model.predict(test_data)[0]
+
+    out[clf]['prognosis'] = label[prognosis]
+    out[clf]['predict_proba'] = np.max(model.predict_proba(test_data)) * 100
+
+
+print(out)
--- nauka_klasyfikatora.py
+++ nauka_klasyfikatora.py
@@ -1,5 +1,10 @@
 import numpy as np
+# models
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 import pickle
@@ -9,16 +14,59 @@ dataset = np.genfromtxt("heart.csv", delimiter=",")
 X = dataset[1:, :-1]
 y = dataset[1:, -1].astype(int)
 
-clf = KNeighborsClassifier()
+# K NAJ
+KNN = KNeighborsClassifier()
 
 kf = KFold(n_splits=5, shuffle=True, random_state=1234)
 scores = []
 for train_index, test_index in kf.split(X):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    clf.fit(X_train, y_train)
-    predict = clf.predict(X_test)
+    KNN.fit(X_train, y_train)
+    predict = KNN.predict(X_test)
     scores.append(accuracy_score(y_test, predict))
 
 
-pickle.dump(clf, open('klasyfikator_chorob_serca.pkl', 'wb'), protocol=4)
+pickle.dump(KNN, open('heart_KNN.pkl', 'wb'), protocol=4)
+
+MLP = MLPClassifier(max_iter=300)
+
+kf = KFold(n_splits=5, shuffle=True, random_state=1234)
+scores = []
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    MLP.fit(X_train, y_train)
+    predict = MLP.predict(X_test)
+    scores.append(accuracy_score(y_test, predict))
+
+
+pickle.dump(MLP, open('heart_MLP.pkl', 'wb'), protocol=4)
+
+SVC = SVC(probability=True, kernel='linear')
+
+kf = KFold(n_splits=5, shuffle=True, random_state=1234)
+scores = []
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    SVC.fit(X_train, y_train)
+    predict = SVC.predict(X_test)
+    scores.append(accuracy_score(y_test, predict))
+
+
+pickle.dump(SVC, open('heart_SVC.pkl', 'wb'), protocol=4)
+
+NB = GaussianNB()
+
+kf = KFold(n_splits=5, shuffle=True, random_state=1234)
+scores = []
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    NB.fit(X_train, y_train)
+    predict = NB.predict(X_test)
+    scores.append(accuracy_score(y_test, predict))
+
+
+pickle.dump(NB, open('heart_NB.pkl', 'wb'), protocol=4)
